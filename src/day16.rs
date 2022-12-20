626b464eb7f35ba89f6dda8d740bd319f7bca07c
Yohan Simard <yoh129.sim@gmail.com>
2022-12-21 00:52:37 +0100

    add day16

--- src/day16.rs
+++ src/day16.rs
@@ -1,40 +1,31 @@
-use std::{
-    collections::HashMap,
-    fmt::{Debug, Display}, fs,
-};
+use std::{collections::HashMap, fmt::{Debug, Display}, fs};
+use std::cmp::{max, min};
+use std::collections::HashSet;
 
 use colored::*;
-use graph::Graph;
 use regex::Regex;
+
+use crate::graph::{CostGraph, Graph};
 
 mod graph;
 
 const DAY: u32 = 16;
+const AA: ValveName = ValveName { name: ['A' as u8; 2] };
+// const DD: ValveName = ValveName { name: ['D' as u8; 2] };
+// const BB: ValveName = ValveName { name: ['B' as u8; 2] };
+// const JJ: ValveName = ValveName { name: ['J' as u8; 2] };
+// const HH: ValveName = ValveName { name: ['H' as u8; 2] };
+// const EE: ValveName = ValveName { name: ['E' as u8; 2] };
+// const CC: ValveName = ValveName { name: ['C' as u8; 2] };
 
 fn main() {
     let sample = fs::read_to_string(format!("samples/day{:02}", DAY)).unwrap_or_default();
     let input = fs::read_to_string(format!("inputs/day{:02}", DAY)).unwrap_or_default();
 
-    println!(
-        "{}{}",
-        "Part 1 sample: ".bold().yellow(),
-        format!("{}", part1(&sample)).bright_white()
-    );
-    println!(
-        "{}{}",
-        "Part 1 input : ".bold().blue(),
-        format!("{}", part1(&input)).bright_white()
-    );
-    println!(
-        "{}{}",
-        "Part 2 sample: ".bold().yellow(),
-        format!("{}", part2(&sample)).bright_white()
-    );
-    println!(
-        "{}{}",
-        "Part 2 input : ".bold().blue(),
-        format!("{}", part2(&input)).bright_white()
-    );
+    println!("{}{}", "Part 1 sample: ".bold().yellow(), format!("{}", part1(&sample)).bright_white());
+    println!("{}{}", "Part 1 input : ".bold().blue(), format!("{}", part1(&input)).bright_white());
+    println!("{}{}", "Part 2 sample: ".bold().yellow(), format!("{}", part2(&sample)).bright_white());
+    println!("{}{}", "Part 2 input : ".bold().blue(), format!("{}", part2(&input)).bright_white());
 }
 
 #[derive(PartialEq, Eq, Hash, Clone, Copy)]
@@ -56,7 +47,7 @@ impl Debug for ValveName {
 
 impl From<&str> for ValveName {
     fn from(s: &str) -> Self {
-        assert!(s.len() == 2);
+        assert_eq!(s.len(), 2);
         ValveName {
             name: [s.bytes().nth(0).unwrap(), s.bytes().nth(1).unwrap()],
         }
@@ -67,18 +58,126 @@ impl From<&str> for ValveName {
 struct Valve {
     name: ValveName,
     flowrate: u32,
-    tunnels: Vec<ValveName>,
+    tunnels: HashMap<ValveName, u8>,
 }
 
-impl Graph<ValveName> for HashMap<ValveName, Valve> {
-    fn neighbors(&self, node: ValveName) -> Vec<ValveName> {
-        self[&node].tunnels.clone()
+type Costs = HashMap<ValveName, HashMap<ValveName, u8>>;
+
+#[derive(Debug)]
+struct PipeNetwork {
+    valves: HashMap<ValveName, Valve>,
+}
+
+impl PipeNetwork {
+    fn simplify(&mut self) {
+        let useless_valves: Costs = self.valves.iter()
+            .filter(|(_, v)| v.flowrate == 0 && v.name != AA)
+            .map(|(&name, v)| (name, v.tunnels.clone()))
+            .collect();
+
+        for valve in self.valves.values_mut() {
+            let mut done = false;
+            let mut visited = HashSet::new();
+            while !done {
+                let useless_tunnel = valve.tunnels.iter()
+                    .filter(|(name, _)| { useless_valves.contains_key(name) }).next();
+
+
+                if let Some((&dest, &cost)) = useless_tunnel {
+                    for (&transitive_dest, &transitive_cost) in useless_valves[&dest].iter()
+                        .filter(|(&n, _)| !visited.contains(&n) && n != valve.name) {
+                        let current = valve.tunnels.entry(transitive_dest).or_insert(100);
+                        *current = min(*current, cost + transitive_cost);
+                    }
+                    valve.tunnels.remove(&dest);
+                    visited.insert(dest);
+                } else {
+                    done = true;
+                }
+            }
+        }
+
+        for useless_valve in useless_valves.keys() {
+            self.valves.remove(useless_valve);
+        }
+    }
+
+    fn compute_costs(&self) -> Costs {
+        let mut costs = HashMap::new();
+        for node in self.valves.keys().copied() {
+            costs.insert(node, self.dijkstra(node));
+        }
+        costs
+    }
+    fn calculate_best_path(&mut self, time: u8, with_elephant: bool) -> u32 {
+        let costs = self.compute_costs();
+        let mut todo: Vec<_> = self.valves.keys().copied().filter(|&v| v != AA).collect();
+        todo.sort_by(|a, b| self.valves[a].flowrate.cmp(&self.valves[b].flowrate));
+        if with_elephant {
+            self.calculate_best_path_elephant_rec(&costs, &todo, AA, time, AA, time)
+        } else {
+            self.calculate_best_path_rec(&costs, &todo, AA, time)
+        }
+    }
+
+    fn calculate_best_path_rec(&mut self, costs: &Costs, todo: &[ValveName], from: ValveName, remaining: u8) -> u32 {
+        let mut best_pressure = 0;
+        let mut todo2 = todo.to_vec();
+        for (i, &node) in todo.iter().enumerate() {
+            let cost = costs[&from][&node] + 1;
+            if cost > remaining {
+                continue;
+            }
+            let remaining_after = remaining - cost;
+            todo2.remove(i);
+            let mut pressure = self.calculate_best_path_rec(costs, &todo2, node, remaining_after);
+            pressure += self.valves[&node].flowrate * remaining_after as u32;
+            best_pressure = max(pressure, best_pressure);
+            todo2.insert(i, node);
+        }
+        best_pressure
+    }
+    fn calculate_best_path_elephant_rec(&mut self, costs: &Costs, todo: &[ValveName], from: ValveName, remaining: u8, other_dest: ValveName, other_remaining: u8) -> u32 {
+        let mut best_pressure = 0;
+        let mut todo2 = todo.to_vec();
+        for (i, &node) in todo.iter().enumerate() {
+            let cost = costs[&from][&node] + 1;
+            if cost > remaining {
+                continue;
+            }
+            let remaining_after = remaining - cost;
+            todo2.remove(i);
+
+            let mut pressure =
+                if other_remaining > remaining_after {
+                    self.calculate_best_path_elephant_rec(costs, &todo2, other_dest, other_remaining, node, remaining_after)
+                } else {
+                    self.calculate_best_path_elephant_rec(costs, &todo2, node, remaining_after, other_dest, other_remaining)
+                };
+            pressure += self.valves[&node].flowrate * remaining_after as u32;
+            best_pressure = max(pressure, best_pressure);
+
+            todo2.insert(i, node);
+        }
+        best_pressure
     }
 }
 
-fn parse_valves(input: &str) -> HashMap<ValveName, Valve> {
+impl Graph<ValveName> for PipeNetwork {
+    fn neighbors(&self, node: ValveName) -> Vec<ValveName> {
+        self.valves[&node].tunnels.keys().copied().collect()
+    }
+}
+
+impl CostGraph<ValveName, u8> for PipeNetwork {
+    fn cost(&self, from: ValveName, to: ValveName) -> u8 {
+        self.valves[&from].tunnels[&to]
+    }
+}
+
+fn parse_valves(input: &str) -> PipeNetwork {
     let re =
-        Regex::new(r"Valve ([A-Z]{2}) has flow rate=(\d+); tunnels lead to valves ([A-Z]{2}(?:, [A-Z]{2})*)").unwrap();
+        Regex::new(r"Valve ([A-Z]{2}) has flow rate=(\d+); tunnels? leads? to valves? ([A-Z]{2}(?:, [A-Z]{2})*)").unwrap();
     let mut valves = HashMap::new();
 
     for m in re.captures_iter(input) {
@@ -86,18 +185,20 @@ fn parse_valves(input: &str) -> HashMap<ValveName, Valve> {
         valves.insert(name, Valve {
             name,
             flowrate: m.get(2).unwrap().as_str().parse().unwrap(),
-            tunnels: m.get(3).unwrap().as_str().split(", ").map(|s| s.into()).collect(),
+            tunnels: m.get(3).unwrap().as_str().split(", ").map(|s| (s.into(), 1)).collect(),
         });
     }
-    valves
+    PipeNetwork { valves }
 }
 
-fn part1(input: &str) -> usize {
-    let valves = parse_valves(input);
-    dbg!(valves);
-    0
+fn part1(input: &str) -> u32 {
+    let mut valves = parse_valves(input);
+    valves.simplify();
+    valves.calculate_best_path(30, false)
 }
 
-fn part2(input: &str) -> usize {
-    0
+fn part2(input: &str) -> u32 {
+    let mut valves = parse_valves(input);
+    valves.simplify();
+    valves.calculate_best_path(26, true)
 }
--- src/graph.rs
+++ src/graph.rs
@@ -1,8 +1,9 @@
-use std::collections::{HashMap, VecDeque};
-use std::fmt::Debug;
+use std::cmp::{min, Ordering, Reverse};
+use std::collections::{BinaryHeap, HashMap, HashSet, VecDeque};
 use std::hash::Hash;
+use std::ops::Add;
 
-pub trait Graph<T: Eq + Hash + Copy + Debug> {
+pub trait Graph<T: Eq + Hash + Copy> {
     fn neighbors(&self, node: T) -> Vec<T>;
 
     fn route(&self, from: T, to: T) -> Option<VecDeque<T>> {
@@ -39,5 +40,49 @@ pub trait Graph<T: Eq + Hash + Copy + Debug> {
             current = visited[&current].unwrap();
         }
         Some(path)
+    }
+}
+
+#[derive(Eq, PartialEq)]
+struct NodeCost<T: Eq, C: Ord> {
+    node: T,
+    cost: C,
+}
+
+impl<T: Eq, C: Ord> Ord for NodeCost<T, C> {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.cost.cmp(&other.cost)
+    }
+}
+
+impl<T: Eq, C: Ord> PartialOrd for NodeCost<T, C> {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cost.cmp(&other.cost))
+    }
+}
+
+pub trait CostGraph<T: Eq + Hash + Copy, C: Ord + Add<Output=C> + Copy + From<u8>>: Graph<T> {
+    fn cost(&self, from: T, to: T) -> C;
+
+    fn dijkstra(&self, from: T) -> HashMap<T, C> {
+        let mut costs = HashMap::new();
+        let mut todo = BinaryHeap::new();
+        let mut visited = HashSet::new();
+
+        todo.push(Reverse(NodeCost { node: from, cost: 0.into() }));
+
+        while let Some(node_cost) = todo.pop() {
+            let cost: C = node_cost.0.cost;
+            let node = node_cost.0.node;
+            visited.insert(node);
+            for &neighbor in self.neighbors(node).iter()
+                .filter(|n| !visited.contains(n)) {
+                let calculated_cost = cost + self.cost(node, neighbor);
+                costs.entry(neighbor).and_modify(|c| *c = min(*c, calculated_cost)).or_insert(calculated_cost);
+                todo.push(Reverse(NodeCost { node: neighbor, cost: calculated_cost }));
+            }
+        }
+
+        costs
     }
 }
